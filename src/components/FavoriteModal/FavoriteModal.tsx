0cafbb1651784c4255b6400236cf8ea4ccb03a65
Igor Sechinov <112351537+igasin@users.noreply.github.com>
2024-03-18 12:28:22 +0300

    feat(pixema): add settimeout for modal add and delete favotite movie

--- src/components/FavoriteModal/FavoriteModal.tsx
+++ src/components/FavoriteModal/FavoriteModal.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Portal, PortalTarget } from 'components/Portal/Portal';
 import { AnimatePresence } from 'framer-motion';
 import { CloseIcon } from 'assets';
@@ -6,22 +7,35 @@ import { messageFavoriteModal } from 'constants/constants';
 import { Close, Message, Title } from './styles';
 import { FavoriteProps } from './FavoriteModal.interface';
 
-export const FavoriteModal = ({ isOpen, toggleModal }: FavoriteProps) => (
-  <Portal target={PortalTarget.MODAL}>
-    <AnimatePresence>
-      {isOpen && (
-      <Message
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <Close onClick={toggleModal}>
-          <CloseIcon stroke={Colors.DARK} />
-        </Close>
-        <Title>{messageFavoriteModal}</Title>
-      </Message>
-      )}
-    </AnimatePresence>
-  </Portal>
-);
+export const FavoriteModal = ({ isOpen, toggleModal }: FavoriteProps) => {
+  useEffect(() => {
+    if (isOpen) {
+      const timer = setTimeout(() => {
+        toggleModal({} as React.MouseEvent<HTMLButtonElement>);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    }
+    return () => {};
+  }, [isOpen, toggleModal]);
+
+  return (
+    <Portal target={PortalTarget.MODAL}>
+      <AnimatePresence>
+        {isOpen && (
+        <Message
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Close onClick={toggleModal}>
+            <CloseIcon stroke={Colors.DARK} />
+          </Close>
+          <Title>{messageFavoriteModal}</Title>
+        </Message>
+        )}
+      </AnimatePresence>
+    </Portal>
+  );
+};
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,9 +4,9 @@ export const OMDB_API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 
 export const BASE_URL = 'https://www.omdbapi.com/';
 
-export const messageDeleteFavoriteModal = 'This movie has been removed from your favorite list';
+export const messageDeleteFavoriteModal = 'Removed movie from your favorite list';
 
-export const messageFavoriteModal = 'This movie has been added to your favorite list';
+export const messageFavoriteModal = 'Added movie to your favorite list';
 
 export const FIREBASE_API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
 
